c6b985cc38c1e6b3b7e3a09b40938c56961633a2
ganebabuk <kganeshbabu.it@gmail.com>
2022-07-03 14:24:44 +0530

    react lib added

---
+++ apps/products-details/pages/product-details.tsx
@@ -0,0 +1,13 @@
+import styles from './index.module.css';
+import { Header } from '@shoppers-hub/header';
+
+export function ProductDetails() {
+    return (
+        <>
+            <Header />
+            <h1>Product Details</h1>
+        </>
+    )
+}
+
+export default ProductDetails;
---
+++ apps/products-list/pages/product-list.tsx
@@ -0,0 +1,13 @@
+import styles from './index.module.css';
+import { Header } from '@shoppers-hub/header';
+
+export function ProductList() {
+    return (
+        <>
+            <Header />
+            <h1>Product List</h1>
+        </>
+    )
+}
+
+export default ProductList;
